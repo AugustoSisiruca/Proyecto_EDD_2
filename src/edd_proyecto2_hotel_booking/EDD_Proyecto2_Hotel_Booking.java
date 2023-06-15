8afed1c40fb65148a78d4bb84ed832048f884812
AugustoSisiruca <sisiruca.augusto@correo.unimet.edu.ve>
2023-06-15 09:40:28 -0400

    📝Primer Repositorio

---
+++ src/edd_proyecto2_hotel_booking/EDD_Proyecto2_Hotel_Booking.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd_proyecto2_hotel_booking;
+
+/**
+ *
+ * @author sisir
+ */
+public class EDD_Proyecto2_Hotel_Booking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
